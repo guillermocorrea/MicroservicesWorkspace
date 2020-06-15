6fb7abc74702254660c9529c4144c2c555f8bcf4
Guillermo Correa <guillermo.correa.99@gmail.com>
2020-06-14 21:32:19 -0500

    Added moderation service

--- blog/comments/index.js
+++ blog/comments/index.js
@@ -19,22 +19,40 @@ app.post('/posts/:id/comments', async (req, res) => {
   const { content } = req.body;
 
   const comments = commentsByPostId[req.params.id] || [];
-  comments.push({ id: commentId, content });
+  const comment = { id: commentId, content, status: 'pending' };
+  comments.push(comment);
   commentsByPostId[req.params.id] = comments;
 
   await axios.post('http://localhost:4005/events', {
     type: 'CommentCreated',
     data: {
-      id: commentId,
-      content,
+      ...comment,
       postId: req.params.id,
     },
   });
   res.status(201).send(comments);
 });
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
   console.log('Received event', req.body.type);
+
+  const { type, data } = req.body;
+  if (type === 'CommentModerated') {
+    const { postId, id, status, content } = data;
+    const comments = commentsByPostId[postId];
+    const comment = comments.find((comment) => comment.id === id);
+    comment.status = status;
+
+    axios.post('http://localhost:4005/events', {
+      type: 'CommentUpdated',
+      data: {
+        id,
+        postId,
+        status,
+        content,
+      },
+    });
+  }
 
   res.send({});
 });

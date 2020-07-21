e1011205447e946f75cde9410551977dc33a14c8
Guillermo Correa <guillermo.correa.99@gmail.com>
2020-07-20 20:36:46 -0500

    feat(auth): scaffold routes

--- ticketing/packages/auth/src/index.ts
+++ ticketing/packages/auth/src/index.ts
@@ -1,11 +1,16 @@
 import express from 'express';
 import { json } from 'body-parser';
+import { meRouter } from './routes/me';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
 
 const app = express();
 app.use(json());
 
-app.get('/api/users/me', (req, res) => {
-  res.send('Hi there');
-});
+app.use(meRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
 
 app.listen(3000, () => console.log('"@ticketing/auth" listening on port 3000'));

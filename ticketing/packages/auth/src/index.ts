3545c7afce51ccdd304cf06ac8a2a17b7cf46777
Guillermo Correa <guillermo.correa.99@gmail.com>
2020-07-20 20:03:07 -0500

    feat: add ticketing scaffold and infra

---
+++ ticketing/packages/auth/src/index.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import { json } from 'body-parser';
+
+const app = express();
+app.use(json());
+
+app.get('/api/users/me', (req, res) => {
+  res.send('Hi there');
+});
+
+app.listen(3000, () => console.log('"@ticketing/auth" listening on port 3000'));
